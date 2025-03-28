162422cfde858d3ef77c0a3bf537f1b2e6e7ebea
Gabriel Drouin <info@gabrieldrouin.com>
2025-03-28 16:23:51 -0400

    basic scaffold in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+
+	defer conn.Close()
+	fmt.Println("Connection succesful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	sig := <-signalChan
+	fmt.Println("Received signal:", sig)
+	fmt.Println("Shutting down Peril server...")
 }
