a3b3e228cb5c798ce8a6cc4f41cee27f4f577647
Gabriel Drouin <info@gabrieldrouin.com>
2025-04-20 07:52:39 -0400

    subscribe + pause/move handlers

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -19,22 +19,36 @@ func main() {
 		log.Fatalf("could not connect to RabbitMQ: %v", err)
 	}
 	defer conn.Close()
+
 	username, err := gamelogic.ClientWelcome()
 	if err != nil {
 		log.Fatalf("could not retrieve username: %v", err)
 	}
-	_, _, err = pubsub.DeclareAndBind(
+
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not create channel: %v", err)
+	}
+
+	if _, _, err := pubsub.DeclareAndBind(
 		conn,
 		routing.ExchangePerilDirect,
 		routing.PauseKey+"."+username,
 		routing.PauseKey,
 		pubsub.SimpleQueueTransient,
-	)
-	if err != nil {
+	); err != nil {
 		log.Fatalf("could not declare and bind: %v", err)
 	}
 
 	gamestate := gamelogic.NewGameState(username)
+
+	if err := pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, routing.PauseKey+"."+username, routing.PauseKey, pubsub.SimpleQueueTransient, handlerPause(gamestate)); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, routing.ArmyMovesPrefix+"."+username, routing.ArmyMovesPrefix+".*", pubsub.SimpleQueueTransient, handlerMove(gamestate, publishCh)); err != nil {
+		fmt.Println(err)
+	}
 
 	for {
 		words := gamelogic.GetInput()
@@ -43,14 +57,17 @@ func main() {
 		}
 		switch words[0] {
 		case "move":
-			_, err := gamestate.CommandMove(words)
+			mv, err := gamestate.CommandMove(words)
 			if err != nil {
 				fmt.Println(err)
 				continue
+			} else if err := pubsub.PublishJSON(publishCh, routing.ExchangePerilTopic, routing.ArmyMovesPrefix+"."+username, mv); err != nil {
+				fmt.Println(err)
+				continue
 			}
+			fmt.Printf("Moved %v units to %s\n", len(mv.Units), mv.ToLocation)
 		case "spawn":
-			err = gamestate.CommandSpawn(words)
-			if err != nil {
+			if err := gamestate.CommandSpawn(words); err != nil {
 				fmt.Println(err)
 				continue
 			}
@@ -65,6 +82,31 @@ func main() {
 			return
 		default:
 			fmt.Println("unknown command")
+		}
+	}
+}
+
+func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.Acktype {
+	return func(ps routing.PlayingState) pubsub.Acktype {
+		defer fmt.Print("> ")
+		gs.HandlePause(ps)
+		return pubsub.Ack
+	}
+}
+
+func handlerMove(gs *gamelogic.GameState, ch *amqp.Channel) func(gamelogic.ArmyMove) pubsub.Acktype {
+	return func(move gamelogic.ArmyMove) pubsub.Acktype {
+		defer fmt.Print("> ")
+		mov := gs.HandleMove(move)
+		if mov == gamelogic.MoveOutComeSafe {
+			return pubsub.Ack
+		} else if mov == gamelogic.MoveOutcomeMakeWar {
+			if err := pubsub.PublishJSON(ch, routing.ExchangePerilTopic, string(routing.WarRecognitionsPrefix)+"."+gs.GetUsername(), mov); err != nil {
+				fmt.Println(err)
+			}
+			return pubsub.NackRequeue
+		} else {
+			return pubsub.NackDiscard
 		}
 	}
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -3,15 +3,23 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type SimpleQueueType int
+type Acktype int
 
 const (
 	SimpleQueueDurable SimpleQueueType = iota
 	SimpleQueueTransient
+)
+
+const (
+	Ack Acktype = iota
+	NackRequeue
+	NackDiscard
 )
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
@@ -23,6 +31,54 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		ContentType: "application/json",
 		Body:        dat,
 	})
+}
+
+func SubscribeJSON[T any](
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType SimpleQueueType,
+	handler func(T) Acktype,
+) error {
+	ch, _, err := DeclareAndBind(conn, exchange, queueName, key, simpleQueueType)
+	if err != nil {
+		return err
+	}
+	dels, err := ch.Consume(queueName, "", false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+	go func() error {
+		for del := range dels {
+			var msg T
+			if err := json.Unmarshal(del.Body, &msg); err != nil {
+				fmt.Printf("Error unmarshaling message: %v\n", err)
+				continue
+			}
+			ack := handler(msg)
+			switch ack {
+			case Ack:
+				if err := del.Ack(false); err != nil {
+					fmt.Printf("error acknowleding message: %v\n", err)
+				}
+				fmt.Println("Ack")
+			case NackRequeue:
+				if err := del.Nack(false, true); err != nil {
+					fmt.Printf("error acknowleding message: %v\n", err)
+				}
+				fmt.Println("NackRequeue")
+			case NackDiscard:
+				if err := del.Nack(false, false); err != nil {
+					fmt.Printf("error acknowleding message: %v\n", err)
+				}
+				fmt.Println("NackDiscard")
+			}
+
+		}
+		return nil
+	}()
+	return nil
 }
 
 func DeclareAndBind(
@@ -43,7 +99,7 @@ func DeclareAndBind(
 	case 1:
 		durable, autoDelete, exclusive = false, true, true
 	}
-	q, err := ch.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	q, err := ch.QueueDeclare(queueName, durable, autoDelete, exclusive, false, amqp.Table{"x-dead-letter-exchange": "peril_dlx"})
 	if err != nil {
 		return nil, amqp.Queue{}, err
 	}
